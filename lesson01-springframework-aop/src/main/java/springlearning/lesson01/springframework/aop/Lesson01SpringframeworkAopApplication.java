c7705c0aa9a7c7dc20c3affbe0d5f1235afb0fc3
yee <dreamloster@hotmail.com>
2022-08-14 18:08:23 +0800

    代码初始化更新

---
+++ lesson01-springframework-aop/src/main/java/springlearning/lesson01/springframework/aop/Lesson01SpringframeworkAopApplication.java
@@ -0,0 +1,13 @@
+package springlearning.lesson01.springframework.aop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lesson01SpringframeworkAopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lesson01SpringframeworkAopApplication.class, args);
+    }
+
+}
